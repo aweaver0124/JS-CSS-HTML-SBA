9b9ce10f4873a4522e0bbd054c011c401c8d191d
Alexandra Weaver <aweaver0124@gmail.com>
2022-11-20 17:51:58 -0600

    completed carousel

---
+++ sba/javascript/app.js
@@ -0,0 +1,40 @@
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+//buttons
+const prevButton = document.querySelector("#prevButton");
+const nextButton = document.querySelector("#nextButton");
+
+// Counter
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+//button listeners
+nextButton.addEventListener('click', function(){
+    // if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter ++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+})
+
+prevButton.addEventListener('click', function(){
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter --;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+})
+
+carouselSlide.addEventListener('transitionend', function(){
+    if (carouselImages[counter].id === 'lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+    }
+    if (carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+    }
+} )
