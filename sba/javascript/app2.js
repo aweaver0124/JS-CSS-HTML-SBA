a518a2a6717454188877f0c4569cbb5babc562cf
aweaver0124 <aweaver0124@gmail.com>
2022-11-21 02:23:54 -0600

    finished recipes page

--- sba/javascript/app2.js
+++ sba/javascript/app2.js
@@ -30,10 +30,43 @@ const CocktailCard = cocktailsOfTheWeek.map(obj => {
    div.classList.add('cocktail-card');
    div.innerHTML = `<h3>${obj.name}</h3>
    <img src=${obj.image} height="350px" width="400px" />
-   <p>Description: ${obj.description}</p>
-   <p>Location: ${obj.location}</p>
-   <p class="ingredients"><strong>Ingredients +</strong></p>
+   <p><strong>Description: </strong>${obj.description}</p>
+   <p><strong>Location: </strong>${obj.location}</p>
+   <ul id="ingredients${obj.id}"><strong>Ingredients +</strong></ul>
+   <span id="like-count-${obj.id}">0 Likes&#128151</span>
    `
    document.querySelector('#recipes-container').appendChild(div);
 })
 
+const getIngredients = cocktailsOfTheWeek.map(obj =>{
+    const ingred = obj.ingredients;
+    ingred.forEach(item => {
+       let li = document.createElement("li");
+       li.classList.add("ingredient");
+       li.innerHTML = item;
+
+       let list = document.querySelector(`#ingredients${obj.id}`)
+       list.appendChild(li)
+    })
+})
+
+// Likes
+
+let likeCountOne = 0;
+let likeCountTwo = 0;
+let likeCountThree = 0;
+
+document.querySelector('#button1').addEventListener('click', (e) => {
+    e.target = likeCountOne++
+   document.querySelector('#like-count-1').innerHTML = likeCountOne + " Likes&#128151"
+})
+
+document.querySelector('#button2').addEventListener('click', (e) => {
+    e.target = likeCountTwo++
+   document.querySelector('#like-count-2').innerHTML = likeCountTwo + " Likes&#128151"
+})
+
+document.querySelector('#button3').addEventListener('click', (e) => {
+    e.target = likeCountThree++
+   document.querySelector('#like-count-3').innerHTML = likeCountThree + " Likes&#128151"
+})
