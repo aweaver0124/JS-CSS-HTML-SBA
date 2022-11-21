2efe38e2075931cbf2dd57db744187021fcde63b
aweaver0124 <aweaver0124@gmail.com>
2022-11-21 13:58:30 -0600

    form 1 validated

--- sba/javascript/app.js
+++ sba/javascript/app.js
@@ -42,3 +42,35 @@ carouselSlide.addEventListener('transitionend', function(){
     }
 } )
 
+document.getElementById('login-signup-button').addEventListener('click', validate);
+
+function validate(e){
+    e.preventDefault();
+    var fn = document.getElementById('fullnameId').value;
+    var user =  document.getElementById('usernameId').value
+    var pass = document.getElementById('passwordId').value;
+    if (fn == null || fn == ""){
+        let error = document.createElement("li");
+        error.classList.add('error-message');
+        error.innerHTML = "Name cannot be blank";
+        let ul = document.getElementById('errors');
+        ul.appendChild(error);
+    }
+    if (user == null || user ==""){
+        let error2 = document.createElement("li");
+        error2.classList.add('error-message');
+        error2.innerHTML = "Username cannot be blank";
+        let ul = document.getElementById('errors');
+        ul.appendChild(error2);
+    }
+    if (pass == null || pass == ""){
+        let error3 = document.createElement("li");
+        error3.classList.add('error-message');
+        error3.innerHTML = "Password cannot be blank";
+        let ul = document.getElementById('errors');
+        ul.appendChild(error3);
+    } 
+    alert(fn + " has signed in!")
+    return true;
+}
+
